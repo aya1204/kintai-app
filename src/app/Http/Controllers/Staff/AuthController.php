91bef59fccf56000f8f0adf6f24fe66a8c6383cd
ayaka <aya_yu7@yahoo.co.jp>
2025-07-22 23:18:28 +0900

    スタッフ認証用コントローラーを追加

---
+++ src/app/Http/Controllers/Staff/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Staff;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * 会員登録、ログイン、ログアウト用のコントローラー
+ */
+class AuthController extends Controller
+{
+    //
+}
