35489ae1730d9f8f0a2bc2abd7f6a7b9ddb8004b
ayaka <aya_yu7@yahoo.co.jp>
2025-07-22 23:39:02 +0900

    管理者によるスタッフの申請一覧・修正申請画面表示・承認用コントローラーを追加

---
+++ src/app/Http/Controllers/Admin/RequestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * 【管理者用】申請一覧画面表示・
+ * 修正申請画面表示・
+ * 承認用コントローラー
+ */
+class RequestController extends Controller
+{
+    //
+}
