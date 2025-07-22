a59a9b4d9afb0f665adcd71fee5b2cbbccf163a0
ayaka <aya_yu7@yahoo.co.jp>
2025-07-22 23:27:16 +0900

    勤怠登録、勤怠確認用コントローラーを追加

---
+++ src/app/Http/Controllers/Staff/AttendanceController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Staff;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * 出勤・休憩・退勤登録、勤怠一覧、勤怠詳細のコントローラー
+ */
+class AttendanceController extends Controller
+{
+    //
+}
