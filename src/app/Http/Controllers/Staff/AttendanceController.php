1d42091ea2e24ef186d827c62f4a155c0cf21529
ayaka <aya_yu7@yahoo.co.jp>
2025-07-23 17:49:29 +0900

    勤怠登録画面表示のindexメソッドを追加

--- src/app/Http/Controllers/Staff/AttendanceController.php
+++ src/app/Http/Controllers/Staff/AttendanceController.php
@@ -10,5 +10,9 @@ use Illuminate\Http\Request;
  */
 class AttendanceController extends Controller
 {
-    //
+    // 勤怠登録画面表示
+    public function index(Request $request)
+    {
+        return view('staff.attendance.work');
+    }
 }
