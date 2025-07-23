e55d2dd8d79c8d436952e3e3b717fc868c553c62
ayaka <aya_yu7@yahoo.co.jp>
2025-07-23 16:59:59 +0900

    新規会員登録機能を実装

--- src/app/Http/Controllers/Staff/AuthController.php
+++ src/app/Http/Controllers/Staff/AuthController.php
@@ -5,14 +5,31 @@ namespace App\Http\Controllers\Staff;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * 会員登録、ログイン、ログアウト用のコントローラー
  */
 class AuthController extends Controller
 {
+    // 会員登録画面表示
     public function register(Request $request)
     {
         return view('staff.auth.register');
+    }
+
+    // 会員登録機能
+    public function create(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/attendance');
     }
 }
