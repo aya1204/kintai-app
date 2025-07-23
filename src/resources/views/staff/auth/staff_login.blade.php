d2d5b5ecc8d730b7aff42e39d80819d133a9bef4
ayaka <aya_yu7@yahoo.co.jp>
2025-07-23 18:47:51 +0900

    ログイン画面表示を追加

---
+++ src/resources/views/staff/auth/staff_login.blade.php
@@ -0,0 +1,66 @@
+<!-- ログイン画面用のbladeファイル -->
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/staff/auth/staff_login.css') }}">
+@endsection
+
+@section('content')
+<!-- 「ログイン情報が登録されていません。」だけ上部に表示 -->
+@if ($errors->has('email') && $errors->first('email') === 'ログイン情報が登録されていません。')
+<div class="alert">
+    {{ $errors->first('email') }}
+</div>
+@endif
+
+<div class="login-form__content">
+    <div class="login-form__heading">
+        <h2>ログイン</h2>
+    </div>
+
+    <form class="form" action="/login" method="post">
+        @csrf
+
+        <!-- メールアドレス -->
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">メールアドレス</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="email" name="email" value="{{ old('email') }}" />
+                </div>
+                <div class="form__error">
+                    <!-- 上部で表示した「ログイン情報〜」はここには表示しない -->
+                    @if ($errors->has('email') && $errors->first('email') !== 'ログイン情報が登録されていません。')
+                    {{ $errors->first('email') }}
+                    @endif
+                </div>
+            </div>
+        </div>
+
+        <!-- パスワード -->
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">パスワード</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="password" name="password">
+                </div>
+                <div class="form__error">
+                    @error('password')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">ログインする</button>
+        </div>
+    </form>
+    <div class="register__link">
+        <a href="/register" class="register__button-submit">会員登録はこちら</a>
+    </div>
+</div>
+@endsection
