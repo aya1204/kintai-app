16dfc993b4fe3e152be7954cac747d88a3ec2ac2
ayaka <aya_yu7@yahoo.co.jp>
2025-07-28 00:27:47 +0900

    スタッフ用AuthController:ログアウトアクションを追加

--- src/app/Http/Controllers/staff/AuthController.php
+++ src/app/Http/Controllers/staff/AuthController.php
@@ -56,4 +56,16 @@ class AuthController extends Controller
         'email' => 'ログイン情報が登録されていません',
     ]);
     }
+
+    /**
+     * ログアウト処理
+     */
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
 }
