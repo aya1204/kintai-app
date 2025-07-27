a11f867e554127a94507e00747343e6b1ca41f4b
ayaka <aya_yu7@yahoo.co.jp>
2025-07-28 00:33:24 +0900

    勤怠登録のルーティングを追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -38,6 +38,14 @@ Route::middleware('auth:web')->name('staff.')->group(function () {
 
     // 出勤登録画面表示
     Route::get('/attendance', [StaffAttendanceController::class, 'index'])->name('attendance.index');
+    // 出勤登録
+    Route::post('/attendance/work-start', [StaffAttendanceController::class, 'workStart'])->name('attendance.workStart');
+    // 休憩入 登録
+    Route::post('/attendance/take-break', [StaffAttendanceController::class, 'takeBreak'])->name('attendance.takeBreak');
+    // 休憩戻 登録
+    Route::post('/attendance/break-return', [StaffAttendanceController::class, 'breakReturn'])->name('attendance.breakReturn');
+    // 退勤登録
+    Route::post('/attendance/work-end', [StaffAttendanceController::class, 'workEnd'])->name('attendance.workEnd');
 
     // 勤怠一覧画面表示
     Route::get('/attendance/list', [StaffAttendanceController::class, 'attendance'])->name('attendance.staff_list');
