442e54add8365827aec059a8e29b04bae067b1aa
ayaka <aya_yu7@yahoo.co.jp>
2025-08-05 14:02:45 +0900

    RequestWorkモデル修正

--- src/app/Models/RequestWork.php
+++ src/app/Models/RequestWork.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Request as RequestModel;
 
 class RequestWork extends Model
 {
@@ -25,7 +26,7 @@ class RequestWork extends Model
     // request(修正申請)と多対1の関係
     public function request()
     {
-        return $this->belongsTo(Request::class);
+        return $this->belongsTo(RequestModel::class);
     }
 
     // requestBreaks(休憩時間の修正申請)と1対多の関係
@@ -36,6 +37,6 @@ class RequestWork extends Model
     // work（出勤・退勤）と1対1の関係
     public function work()
     {
-        return $this->hasOne(Request::class, 'work_id', 'work_id');
+        return $this->hasOne(RequestModel::class, 'work_id', 'work_id');
     }
 }
