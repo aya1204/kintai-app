cd2faa62805e115623501e82aab98059ccfcb303
ayaka <aya_yu7@yahoo.co.jp>
2025-08-05 17:19:24 +0900

    勤怠一覧画面viewコメントアウトを追加

--- src/resources/views/staff/attendance/list.blade.php
+++ src/resources/views/staff/attendance/list.blade.php
@@ -27,7 +27,7 @@
     $nextMonth = $carbonMonth->copy()->addMonth()->format('Y-m');
     @endphp
 
-    {{-- 勤怠一覧を見る月を選択する行 --}}
+    {{-- 勤怠一覧：月の切り替え(前月・当月・翌月) --}}
     <div class="month-list-form">
         <a class="previous-month" href="{{ route('staff.attendance.list', ['month' => $prevMonth]) }}"><img class="left-arrow-icon" src="{{ asset('storage/images/arrow.png') }}" alt="calender">前月</a>
         <div class="calender-title">
@@ -48,21 +48,24 @@
         </div>
         {{-- 勤怠データの表示 --}}
         @php
+        // 月の最初の日と最後の日
         $startOfMonth = $carbonMonth->copy()->startOfMonth();
         $endOfMonth = $carbonMonth->copy()->endOfMonth();
         @endphp
 
+        {{-- 各日付の勤怠情報を1日ずつループで表示 --}}
         @for ($date = $startOfMonth->copy(); $date->lte($endOfMonth); $date->addDay())
         @php
+        // 表示中の日付の勤怠データを取得
         $attendance = $attendances->firstWhere('date', $date->toDateString());
 
-        // 休憩時間合計
+        // 休憩時間の合計
         $breakMinutes = $attendance && $attendance->breaks
         ? $attendance->breaks->sum(function ($break) {
         return \Carbon\Carbon::parse($break->start_time)->diffInMinutes(\Carbon\Carbon::parse($break->end_time));
         }) : 0;
 
-        // 勤務時間合計
+        // 勤務時間合計(休憩時間を引いた時間、分単位で)
         $workMinutes = ($attendance && $attendance->start_time && $attendance->end_time)
         ? \Carbon\Carbon::parse($attendance->start_time)->diffInMinutes(\Carbon\Carbon::parse($attendance->end_time)) - $breakMinutes
         : 0;
@@ -77,20 +80,33 @@
         : '';
         @endphp
 
-        {{-- 月末日を判定して月末日だけクラスメイをつける --}}
+        {{-- 月末日を判定して月末日だけクラス名をつける --}}
         <div class="work-date-list-form {{ $date->isSameDay($endOfMonth) ? 'last-day' : ''}}">
             {{-- 各勤怠データの表示 --}}
             @php
+            // 曜日を日本語で取得
             $weekdays = ['日', '月', '火', '水', '木', '金', '土'];
             $dayOfWeek= $weekdays[$date->dayOfWeek];
             @endphp
+
+            {{-- 日付と曜日 --}}
             <div class="date">
                 {{ $date->format('m/d')}} ({{ $dayOfWeek }})
             </div>
+
+            {{-- 出勤時間 --}}
             <div class="work-start">{{ $attendance && $attendance->start_time ? \Carbon\Carbon::parse($attendance->start_time)->format('H:i') : '' }}</div>
+
+            {{-- 退勤時間 --}}
             <div class="work-end">{{ $attendance && $attendance->end_time ? \Carbon\Carbon::parse($attendance->end_time)->format('H:i') : '' }}</div>
+
+            {{-- 休憩時間合計 --}}
             <div class="breaks">{{ $attendance ? $breakHours : '' }}</div>
+
+            {{-- 勤務時間合計 --}}
             <div class="total">{{ $attendance ? $workHours : '' }}</div>
+
+            {{-- 詳細ページへのリンク --}}
             <div class="detail">
                 <a href="{{ route('staff.attendance.detail', ['work' => $attendance ? $attendance->id : '0', 'date' => $date->toDateString()]) }}" class="detail-link">詳細</a>
             </div>
