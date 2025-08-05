2504c1d0a034a4ce06fb0011cd80650d2dea525e
ayaka <aya_yu7@yahoo.co.jp>
2025-08-05 23:09:54 +0900

    AttendanceRequest:バリデーションメッセージの追加

--- src/app/Http/Requests/AttendanceRequest.php
+++ src/app/Http/Requests/AttendanceRequest.php
@@ -36,11 +36,18 @@ class AttendanceRequest extends FormRequest
     public function messages()
     {
         return [
+            'start_time.required' => '出勤時間を入力してください',
+            'start_time.date_format' => '出勤時間の形式が正しくありません（例:09:00）',
             'start_time.before' => '出勤時間もしくは退勤時間が不適切な値です',
+            'end_time.required' => '退勤時間を入力してください',
+            'end_time.date_format' => '退勤時間の形式が正しくありません（例:18:00）',
             'end_time.after' => '出勤時間もしくは退勤時間が不適切な値です',
-            'breaks.*.start_time.date_format' => '休憩開始時間の形式が正しくありません（例: 13:00）。',
-            'breaks.*.end_time.date_format' => '休憩終了時間の形式が正しくありません（例: 14:00）。',
+            'breaks.*.start_time.date_format' => '休憩開始時間の形式が正しくありません（例: 12:00）。',
+            'breaks.*.end_time.date_format' => '休憩終了時間の形式が正しくありません（例: 13:00）。',
+            'breaks.*.end_time.required_with' => '休憩開始時間が指定されている場合、休憩終了時間も指定してください',
             'remark.required' => '備考を記入してください',
+            'remark.string' => '備考は文字で入力してください',
+            'remark.max' => '備考は255文字以内で入力してください',
         ];
     }
 
