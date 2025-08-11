80a33a7f713382f00f0d7bae18dfa4d3a5bd5be3
ayaka <aya_yu7@yahoo.co.jp>
2025-08-12 00:31:16 +0900

    ログイン処理のルートを修正

--- src/app/Http/Controllers/admin/AuthController.php
+++ src/app/Http/Controllers/admin/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
         if (Auth::guard('admin')->attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect()->route('attendance.list');
+            return redirect()->route('admin.attendance.list');
         }
 
         return back()->withErrors([
