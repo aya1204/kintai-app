1a3f7940385d889339c992393453c86a8f4430d0
ayaka <aya_yu7@yahoo.co.jp>
2025-08-16 00:34:11 +0900

    管理者スタッフ一覧画面を追加

--- src/app/Http/Controllers/admin/UserController.php
+++ src/app/Http/Controllers/admin/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 /**
  * 【管理者用】スタッフ一覧画面表示・
@@ -11,5 +12,10 @@ use Illuminate\Http\Request;
  */
 class UserController extends Controller
 {
-    //
+    // スタッフ一覧画面を表示
+    public function index(Request $request)
+    {
+        $users = User::orderBy('name')->get();
+        return view('admin.user.staff_list', compact('users'));
+    }
 }
---
+++ src/resources/views/admin/user/staff_list.blade.php
@@ -0,0 +1,42 @@
+{{-- スタッフ一覧画面を表示するBlade --}}
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/admin/user/staff_list.css') }}">
+@endsection
+
+@section('content')
+
+@if(session('success'))
+<div class="alert-success">
+    {{ session('success') }}
+</div>
+@endif
+
+<div class="staff-list-page">
+    <ul class="staff-list-page-title">
+        <li class="staff-list-page-title">スタッフ一覧</li>
+    </ul>
+
+    <div class="staff-list-form">
+        <div class="staff-list-title-form">
+            <p class="name-title">名前</p>
+            <p class="email-title">メールアドレス</p>
+            <p class="detail-title">月次勤怠</p>
+        </div>
+        @foreach($users as $user)
+        <div class="staff-list-row {{ $loop->last ? 'last-staff' : '' }}">
+            <p class="user-name">
+                {{ $user->name }}
+            </p>
+            <p class="email">
+                {{ $user->email }}
+            </p>
+            <p class="detail">
+                <a class="detail-link" href="{{ route('admin.attendance.list', ['user_id' => $user->id]) }}">詳細</a>
+            </p>
+        </div>
+        @endforeach
+    </div>
+</div>
+@endsection
