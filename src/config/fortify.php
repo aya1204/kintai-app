512fa693c405338293c6a28e0cbd2c8b3a39153a
ayaka <aya_yu7@yahoo.co.jp>
2025-08-26 23:39:13 +0900

    スタッフ用：メール認証誘導画面を表示

--- src/config/fortify.php
+++ src/config/fortify.php
@@ -168,7 +168,7 @@ return [
     'features' => [
         Features::registration(),
         Features::resetPasswords(),
-        // Features::emailVerification(),
+        Features::emailVerification(),
         Features::updateProfileInformation(),
         Features::updatePasswords(),
         Features::twoFactorAuthentication([
