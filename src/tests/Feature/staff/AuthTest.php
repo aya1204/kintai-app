8a24c34e0946ac6a6855648e4261b2ccedf30cd0
ayaka <aya_yu7@yahoo.co.jp>
2025-08-31 13:53:04 +0900

    【スタッフ用】ログインに関するテスト用のコード実装

--- src/tests/Feature/staff/AuthTest.php
+++ src/tests/Feature/staff/AuthTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\staff;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 /**
  * 【スタッフ用】登録・ログインのテスト
@@ -66,7 +67,7 @@ class AuthTest extends TestCase
     /**
      * パスワードと確認用パスワードが一致しない場合のバリデーションテスト
      */
-    public function testRegisterFailsWhenPasswordComfirmationDoesNotMatch()
+    public function testRegisterFailsWhenPasswordConfirmationDoesNotMatch()
     {
         $response = $this->post('/register', [
             'name' => 'テストユーザー',
@@ -113,5 +114,64 @@ class AuthTest extends TestCase
 
         // ログインされているか
         $this->assertAuthenticatedAs($user);
+    }
+
+    /**
+     * メールアドレス未入力の場合のバリデーションテスト
+     */
+    public function testLoginFailsWhenEmailIsEmpty()
+    {
+        $response = $this->from('/login')->post('/login', [
+            'email' => '',
+            'password' => 'password',
+        ]);
+
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors(['email' => 'メールアドレスを入力してください']);
+        $this->assertGuest(); // ログイン失敗時に誤って認証されないようチェック
+    }
+
+    /**
+     * パスワード未入力の場合のバリデーションテスト
+     */
+    public function testLoginFailsWhenPasswordIsEmpty()
+    {
+        $response = $this->from('/login')->post('/login', [
+            'email' => 'testabc@example.com',
+            'password' => '',
+        ]);
+
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors(['password' => 'パスワードを入力してください']);
+        $this->assertGuest(); // ログイン失敗時に誤って認証されないようチェック
+    }
+
+    /**
+     * 登録していないメールアドレスやパスワードでログインした場合のバリデーションテスト
+     */
+    public function testLoginFailsWithInvalidCredentials()
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $invalidCases = [
+            ['email' => 'testabc@example.com',
+            'password' => 'password'], // メールアドレスが間違っている場合
+            ['email' => 'test@example.com',
+            'password' => 'password123'], // パスワードが間違っている場合
+            ['email' => 'test123@example.com',
+            'password' => 'password12345'], // どちらも間違っている場合
+        ];
+
+        foreach ($invalidCases as $case) {
+            $response = $this->from('/login')->post('/login', $case);
+            $response->assertRedirect('/login');
+            $response->assertSessionHasErrors([
+                'email' => 'ログイン情報が登録されていません'
+            ]);
+            $this->assertGuest(); // ログイン失敗時に誤って認証されないようチェック
+        }
     }
 }
