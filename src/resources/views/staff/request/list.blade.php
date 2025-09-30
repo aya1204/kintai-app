c3253efc3f4d63739ac8c23f9196afabe81391fb
ayaka <aya_yu7@yahoo.co.jp>
2025-09-30 23:14:11 +0900

    承認待ちタブと承認済みタブを

--- src/resources/views/staff/request/list.blade.php
+++ src/resources/views/staff/request/list.blade.php
@@ -11,8 +11,8 @@
         <li class="list-title">申請一覧</li>
     </ul>
     <div class="tab-buttons">
-        <a href="{{ route('staff.request.list', ['tab' => 'wait'])}}" class="button-submit {{ request('tab', 'wait') === 'wait' ? 'active' : '' }}">承認待ち</a>
-        <a href="{{ route('staff.request.list', ['tab' => 'clear'])}}" class="button-submit {{ request('tab', 'clear') === 'clear' ? 'active' : '' }}">承認済み</a>
+        <a href="{{ route('staff.request.list', ['tab' => 'wait'])}}" class="button-submit {{ $tab === 'wait' ? 'active' : '' }}">承認待ち</a>
+        <a href="{{ route('staff.request.list', ['tab' => 'clear'])}}" class="button-submit {{ $tab === 'clear' ? 'active' : '' }}">承認済み</a>
     </div>
 
     <div class="request-list-form">
